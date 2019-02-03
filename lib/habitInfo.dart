2a6accde251662f0d58af9cfce9c542e05313725
OPelham <oliverpelham@gmail.com>
2019-02-03 15:40:11 +1300

    added post method for new habit

--- lib/habitInfo.dart
+++ lib/habitInfo.dart
@@ -1,9 +1,10 @@
 class habitInfo {
 
+  final String id;
   final String title;
   final String description;
 
-  habitInfo(this.title, this.description);
+  habitInfo(this.id, this.title, this.description);
 
   //could add comparator here
 }
--- lib/tools.dart
+++ lib/tools.dart
@@ -3,7 +3,8 @@ import 'package:http/http.dart' as db;
 import 'dart:convert';
 
 class tools {
-  List<habitInfo> habitList = [];
+  //should this really be static?
+  static List<habitInfo> habitList = [];
 
   //get all habits
 
@@ -15,7 +16,26 @@ class tools {
       'title': title,
       'description': description
     };
-    db.post('https://habitapp-a0e21.firebaseio.com/habits.json', body: json.encode(newHabit));
-    
+    db.post('https://habitapp-a0e21.firebaseio.com/habits.json', body: json.encode(newHabit)).then((db.Response response) {
+      //getting body of response
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      //responseData json contains only name which is used for the id
+      final Habit = new habitInfo(responseData['name'], title, description);
+      habitList.add(Habit);
+    });
+  }
+
+
+
+  //modify existing habit
+
+  //delete existing habit
+
+  //get existing habit/get all?
+  static void fetchHabits() {
+    db.get('https://habitapp-a0e21.firebaseio.com/habits.json' )
+        .then((db.Response response) {
+
+    });
   }
 }
